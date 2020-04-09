07b08495678f1c4b13fe4f9634cf571b3aeb13e4
Shreyas <shreyas.karandikar@gmail.com>
2020-04-08 21:35:09 -0700

    Creating a user, signin/login logout, Intent (New Activity)
    First Activity created with login and sign in.

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -1,7 +1,9 @@
 package com.example.instagramclone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.SigningInfo;
 import android.os.Bundle;
@@ -9,122 +11,105 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 import com.parse.http.ParseHttpResponse;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.List;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
-    private Button btnSave;
-    private EditText punchPower, punchSpeed,kickPower,kickSpeed;
-    private TextView getData;
-    private Button btnAllKickBoxers;
-    private Button btnNextActivity;
-    private String allKickBoxers;
+   private Button btnSignUp;
+   private Button btnLogin;
+
+   private EditText txtUsername,txtPassword,txtPassword2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAllKickBoxers=findViewById(R.id.btnGetAllKB);
-        btnSave = findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(SignUp.this);
-        btnNextActivity=findViewById(R.id.btnNextActivity);
-        kickPower=findViewById(R.id.kickPower);
-        kickSpeed=findViewById(R.id.kickSpeed);
-        punchPower=findViewById(R.id.punchPower);
-        punchSpeed=findViewById(R.id.punchSpeed);
 
-        getData=findViewById(R.id.getData);
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnLogin=findViewById(R.id.btnLogin);
 
-        getData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final ParseQuery<ParseObject>parseQuery= ParseQuery.getQuery("KickBoxer");
-                parseQuery.getInBackground("khMjRU3XXb", new GetCallback<ParseObject>() {
+        txtPassword=findViewById(R.id.txtPassword);
+        txtPassword2=findViewById(R.id.txtPassword2);
+        txtUsername=findViewById(R.id.txtEmail);
+
+        btnSignUp.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        if(ParseUser.getCurrentUser()!=null){
+            Log.i("**********","!!!!!!!!!!!!!1");
+            ParseUser.getCurrentUser().logOut();
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                ParseUser.logInInBackground(txtUsername.getText().toString(), txtPassword.getText().toString(), new LogInCallback() {
                     @Override
-                    public void done(ParseObject object, ParseException e) {
-                        if(object!=null &&  e==null){
-                           getData.setText(object.get("kickSpeed")+"");
+                    public void done(ParseUser user, ParseException e) {
 
+                        if(user != null && e == null){
+                            FancyToast.makeText(SignUp.this,ParseUser.getCurrentUser().getUsername()+" logged in.",FancyToast.SUCCESS,FancyToast.LENGTH_SHORT,true).show();
                         }
                     }
                 });
-            }
-        });
 
-        btnNextActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(SignUp.this, SignUpLoginActivity.class);
-                startActivity(intent);
-            }
-        });
+                break;
+
+            case R.id.btnSignUp:
+
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Signing Up " + txtUsername.getText()+toString());
+                progressDialog.show();
 
 
-        btnAllKickBoxers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                allKickBoxers="";
-                ParseQuery<ParseObject> queryAll= ParseQuery.getQuery("KickBoxer");
-//               queryAll.whereGreaterThanOrEqualTo("kickSpeed",1);
-               queryAll.whereGreaterThan("punchPower",2);
-//               queryAll.setLimit(1);
+                final ParseUser appUser=new ParseUser();
+                appUser.setUsername(txtUsername.getText().toString());
+                appUser.setPassword(txtPassword.getText().toString());
 
-                queryAll.findInBackground(new FindCallback<ParseObject>() {
-                    @Override
-                    public void done(List<ParseObject> objects, ParseException e) {
-                            if(e == null){
-                                if(objects.size() > 0){
-                                    for(ParseObject object:objects){
-                                        allKickBoxers+=object.get("punchPower") + " \n";
-                                    }
-                                    FancyToast.makeText(SignUp.this, allKickBoxers, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                                }
-                                else{
-                                    FancyToast.makeText(SignUp.this, "KickBOxer object has been saved!", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-                                }
+
+                if(txtPassword2.getText().toString().equals(txtPassword.getText().toString()) &&
+                        txtPassword2.getText().toString() != "" && txtPassword.getText().toString()!=null) {
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(SignUp.this, appUser.getUsername() + " has signed in", FancyToast.SUCCESS, FancyToast.LENGTH_SHORT, true).show();
+                            } else {
+                                FancyToast.makeText(SignUp.this, e.getMessage() + "", FancyToast.ERROR, FancyToast.LENGTH_SHORT, true).show();
                             }
-                    }
-                });
-            }
-        });
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        try {
-            final ParseObject kickboxer = new ParseObject("KickBoxer");
-            kickboxer.put("punchPower", Integer.parseInt(punchPower.getText().toString()));
-            kickboxer.put("kickSpeed", Integer.parseInt(kickSpeed.getText().toString()));
-            kickboxer.put("punchSpeed", Integer.parseInt(punchSpeed.getText().toString()));
-            kickboxer.put("kickPower", Integer.parseInt(kickPower.getText().toString()));
-            kickboxer.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null) {
-                        FancyToast.makeText(SignUp.this, "KickBOxer object has been saved!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-
-                    } else {
-                        FancyToast.makeText(SignUp.this, "Error", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-                    }
+                        }
+                    });
+                }else{
+                    FancyToast.makeText(SignUp.this,"Passwords do not match",FancyToast.ERROR,FancyToast.LENGTH_SHORT,true).show();
                 }
-            });
-        } catch (Exception e){
-            FancyToast.makeText(SignUp.this,e+"",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                progressDialog.dismiss();
+
+                break;
         }
     }
 }
+
+
+
 
 
